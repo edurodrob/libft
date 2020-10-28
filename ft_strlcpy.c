0e1629be1a6463cac178c365e599285d0b6293a4
Eduardo Rodriguez robles <erodrigu@c1r1s5.42madrid.com>
2020-10-28 21:29:49 +0100

    DEF

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,32 +6,32 @@
 /*   By: erodrigu <erodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 19:26:55 by erodrigu          #+#    #+#             */
-/*   Updated: 2020/10/26 20:55:20 by erodrigu         ###   ########.fr       */
+/*   Updated: 2020/10/28 21:28:34 by erodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    size_t  i;
+	size_t	i;
 
-if (!dst || !src)
-return (0);
-    i = 0;
-    while (src[i])
-i++;
-    if (dstsize == 0)
-return (i);
-    i = 0;
-    while (src[i] && i < dstsize - 1)
-    {
-dst[i] = src[i];
-i++;
-    }
-    dst[i] = '\0';
-    i = 0;
-    while (src[i])
-i++;
-return (i);
-} 
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	while (src[i])
+		i++;
+	if (dstsize == 0)
+		return (i);
+	i = 0;
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
