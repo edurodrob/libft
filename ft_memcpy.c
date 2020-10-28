4817d117c3afe0608159297d44b3f9b4d4f148ce
Eduardo <edurodrob@hotmail.com>
2020-10-28 15:38:09 +0100

    asd

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	dest = (char *)dst;
 	source = (char *)src;
 	i = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		dest[i] = source[i];
