9d0398ca1c96646e104fe48419665a6464ed4a7c
Eduardo <edurodrob@hotmail.com>
2020-10-28 16:04:17 +0100

    normi

--- libft.h
+++ libft.h
@@ -1,10 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erodrigu <erodrigu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/26 19:26:55 by erodrigu          #+#    #+#             */
+/*   Updated: 2020/10/26 20:55:20 by erodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 #define LIBFT_H
 
-////////////
-#include <stdio.h>
-#include <ctype.h>
-/////////////
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,17 +22,17 @@ void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memchr(const void *str, int c, size_t n);
-int		ft_memcmp(const void *str1, const void *str2, size_t n);
+void    *ft_memchr(const void *str, int c, size_t n);
+int     ft_memcmp(const void *str1, const void *str2, size_t n);
 size_t  ft_strlen(const char *str);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strchr(const char *str, int c);
 char    *ft_strrchr(const char *str, int c);
 char    *ft_strnstr(const char *str, const char *to_find, size_t len);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
 int     ft_atoi(const char *str);
-int		ft_isalpha(int c);
+int     ft_isalpha(int c);
 int     ft_isdigit(int c);
 int     ft_isalnum(int c);
 int     ft_isascii(int c);
