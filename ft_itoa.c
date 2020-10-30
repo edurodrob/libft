be6b182ee070506c749f04a0ec0868f3537eece1
Eduardo Rodriguez robles <erodrigu@c1r2s2.42madrid.com>
2020-10-30 14:10:22 +0100

    def

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,50 +6,50 @@
 /*   By: erodrigu <erodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 19:10:10 by erodrigu          #+#    #+#             */
-/*   Updated: 2020/10/26 20:12:34 by erodrigu         ###   ########.fr       */
+/*   Updated: 2020/10/30 14:09:41 by erodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- static int            ft_nblen(unsigned int n)
+static int	ft_nblen(unsigned int n)
 {
-    unsigned int    i;
+	unsigned int	i;
 
-    i = 0;
-    while (n >= 10)
-    {
-        n /= 10;
-        i++;
-    }
-    return (i + 1);
+	i = 0;
+	while (n >= 10)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i + 1);
 }
 
-char                *ft_itoa(int n)
+char		*ft_itoa(int n)
 {
-    char            *dest;
-    unsigned int    len;
-    unsigned int    nb;
-    unsigned int    i;
+	char			*dest;
+	unsigned int	len;
+	unsigned int	nb;
+	unsigned int	i;
 
-    nb = (n < 0 ? -n : n);
-    len = ft_nblen(nb);
-    i = 0;
-    if (!(dest = (char *)malloc(sizeof(char) * len + 1 + (n < 0 ? 1 : 0))))
-        return (NULL);
-    if (n < 0)
-    {
-        dest[i] = '-';
-        len++;
-    }
-    i = len - 1;
-    while (nb >= 10)
-    {
-        dest[i] = nb % 10 + 48;
-        nb /= 10;
-        i--;
-    }
-    dest[i] = nb % 10 + 48;
-    dest[len] = '\0';
-    return (dest);
+	nb = (n < 0 ? -n : n);
+	len = ft_nblen(nb);
+	i = 0;
+	if (!(dest = (char *)malloc(sizeof(char) * len + 1 + (n < 0 ? 1 : 0))))
+		return (NULL);
+	if (n < 0)
+	{
+		dest[i] = '-';
+		len++;
+	}
+	i = len - 1;
+	while (nb >= 10)
+	{
+		dest[i] = nb % 10 + 48;
+		nb /= 10;
+		i--;
+	}
+	dest[i] = nb % 10 + 48;
+	dest[len] = '\0';
+	return (dest);
 }
